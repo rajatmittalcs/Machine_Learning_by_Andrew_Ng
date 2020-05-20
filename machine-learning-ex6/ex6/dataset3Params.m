dcb86d975a9e4319e46c80014b800628e38d3070
Rajat Mittal <64283566+rajatmittalcs@users.noreply.github.com>
2020-05-20 19:53:44 +0530

    ex 6 completed

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -22,24 +22,39 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+
+%C_sigma is the set of values to find the best combo
+
 C_sigma=[0.01,0.03,0.1,0.3,1,3,10,30];
 errors=zeros(size(C_sigma,2),size(C_sigma,2));
+%errors is a matrix to collect all the error values in below iterations
+
 for i=1:numel(C_sigma)
 for j=1:numel(C_sigma)
 
 C=C_sigma(i);
 sigma=C_sigma(j);
+
+%Training the model on X and y data with iterations of C and sigma
 model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+
+%Predicting values on cross validation set
 predictions=svmPredict(model,Xval);
+
+%Caluclating error in above predictions
 error=mean(double(predictions ~= yval));
 
+%Adding all the errors in one erros matrix
 errors(i,j)=error;
 
 
 end
 end
 
+%Finding the minimum error value
 min_error=min(min(errors));
+
+%Finding indexes of the min error value to find the best values of C n sigma
 [indX,indY]=find(errors==min_error);
 C=C_sigma(indX(1));
 sigma=C_sigma(indY(1));
